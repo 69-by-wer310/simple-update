973175c85531ccd6d4a61bddec0b39d722c0c880
wer310 <77055011+wer310@users.noreply.github.com>
2023-02-09 13:34:59 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,3 @@
+from setuptools import *
+from SimpleUpdates import __version__ as version
+setup(name="simple-updates", author="69-by-wer310", author_email="69.by.wer310@gmail.com", version=version, packages=find_packages())
